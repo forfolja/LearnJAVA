c7dbaeffeb8b0a2a4ee824ba83f562ac5d05c5a1
Chris Xu <xuchen@jiazhiyi.com>
2023-01-03 10:21:21 +0800

    update the static method and class invoke

--- src/com/HeadFirstJAVA/Duck.java
+++ src/com/HeadFirstJAVA/Duck.java
@@ -1,12 +1,27 @@
 package com.HeadFirstJAVA;
 
 public class Duck {
-    int size;
-    public Duck(){
+    private static int size;
+   /* public Duck(){
         System.out.println("Quack");
     }
 
     public void setSize(int newSize) {
         size = newSize;
     }
+   public static void main(String[] args) {
+     System.out.println(
+             "Size of the duck is "+size
+     );
+   }*/
+   public static void main(String[] args) {
+       System.out.println("Size is "+ getSize());
+   }
+
+    public static void setSize(int size) {
+        Duck.size = size;
+    }
+    public static int getSize() {
+       return size;
+   }
 }

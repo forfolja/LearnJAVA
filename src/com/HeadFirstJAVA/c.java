03dc5fc2db01e4be6ece3ac11daae6a80cc27c28
Chris Xu <xuchen@jiazhiyi.com>
2023-01-03 17:28:30 +0800

    update math learn lessons content

---
+++ src/com/HeadFirstJAVA/c.java
@@ -0,0 +1,11 @@
+package com.HeadFirstJAVA;
+import java.util.Calendar;
+public class c {
+    Calendar c = Calendar.getInstance();
+c.set (2004,1,7,15,40);
+    long day1 = c.getTimeInMillis();
+    day1 += 1000*60*60;
+
+    c.setTimeInMillis(day1);
+
+}

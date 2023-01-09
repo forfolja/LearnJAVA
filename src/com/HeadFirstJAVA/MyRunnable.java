bf5a98db90b95349327432e3d8d2a207c7992076
Chris Xu <xuchen@jiazhiyi.com>
2023-01-09 14:49:06 +0800

    Update the Thread and sleep method used

---
+++ src/com/HeadFirstJAVA/MyRunnable.java
@@ -0,0 +1,24 @@
+package com.HeadFirstJAVA;
+
+public class MyRunnable implements Runnable{
+
+    public void run (){
+        go();
+    }
+    public void go (){
+        try{
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ex)
+        {
+            ex.printStackTrace();
+        };
+        doMore();
+    }
+
+
+
+    public void doMore(){
+        System.out.println("top o' the stack");
+    }
+}
---
+++ src/com/HeadFirstJAVA/NetTest.java
@@ -0,0 +1,15 @@
+package com.HeadFirstJAVA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/*public class NetTest {
+    Socket chatSocket = new Socket("127.0.0.1",5000);
+    InputStreamReader stream = new InputStreamReader(chatSocket.getInputStream());
+    BufferedReader reader = new BufferedReader(stream);
+    String message = reader.readLine();
+
+}
+
+*/
